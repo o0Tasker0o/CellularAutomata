b3b9ab4b6df30f03937368e501ec3ae3d3a4500c
James Tasker <james_tasker1@hotmail.com>
2015-11-06 13:20:11 +0000

    Add solution
    Signed-off-by: James Tasker <james_tasker1@hotmail.com>

---
+++ CellularAutomataLib/Field.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace CellularAutomataLib
+{
+    public class Field
+    {
+        bool[,] mCells;
+
+        public int Width
+        {
+            get
+            {
+                return mCells.GetLength(0);
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return mCells.GetLength(1);
+            }
+        }
+
+        public Field(UInt32 width, UInt32 height)
+        {
+            if (0 == width || 0 == height)
+            {
+                throw new ArgumentOutOfRangeException("Width and height must not be 0");
+            }
+
+            mCells = new bool[width, height];
+        }
+
+        public bool GetCell(UInt32 x, UInt32 y)
+        {
+            if (x >= Width || y >= Height)
+            {
+                return false;
+            }
+
+            return mCells[x, y];
+        }
+
+        public void SetCell(UInt32 x, UInt32 y)
+        {
+            if (x >= Width || y >= Height)
+            {
+                throw new ArgumentOutOfRangeException("Indices must be within field dimensions");
+            }
+
+            mCells[x, y] = true;
+        }
+
+        public void Update()
+        {
+            bool [,] updateCells =  (bool [,]) mCells.Clone();
+
+            for(int x = 0; x < Width; ++x)
+            {
+                for(int y = 0; y < Height; ++y)
+                {
+                    int neighbourCount = GetNeighbourCount(x, y);
+
+                    if(neighbourCount < 2 || neighbourCount > 3)
+                    {
+                        updateCells[x, y] = false;
+                    }
+
+                    if(neighbourCount == 3)
+                    {
+                        updateCells[x, y] = true;
+                    }
+                }
+            }
+
+            mCells = (bool[,]) updateCells.Clone();
+        }
+
+        private int GetNeighbourCount(int x, int y)
+        {
+            int count = 0;
+
+            for (int i = -1; i < 2; ++i)
+            {
+                for (int j = -1; j < 2; ++j)
+                {
+                    if(!(i == 0 && j == 0))
+                    {
+                        try
+                        {
+                            if(GetCell((UInt32) (x + i), (UInt32) (y + j)))
+                            {
+                                ++count;
+                            }
+                        }
+                        catch(ArgumentOutOfRangeException)
+                        {
+
+                        }
+                    }
+                }
+            }
+                
+            return count;
+        }
+    }
+}
---
+++ CellularAutomataLibTests/FieldTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CellularAutomataLib;
+
+namespace CellularAutomataLibTests
+{
+    [TestClass]
+    public class FieldTests
+    {
+        [TestMethod]
+        public void FieldTakesWidthAndHeight()
+        {
+            Field field = new Field(1, 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void FieldThrowsExceptionFor0Width()
+        {
+            Field field = new Field(0, 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void FieldThrowsExceptionFor0Height()
+        {
+            Field field = new Field(1, 0);
+        }
+
+        [TestMethod]
+        public void FieldIsConstructedWithAllOffCells()
+        {
+            Field field = new Field(1, 1);
+
+            Assert.IsFalse(field.GetCell(0, 0));
+        }
+
+        [TestMethod]
+        public void FieldThrowsExceptionWhenGettingCellsBeyondWidth()
+        {
+            Field field = new Field(1, 1);
+            Assert.IsFalse(field.GetCell(1, 0));
+        }
+
+        [TestMethod]
+        public void FieldThrowsExceptionWhenGettingCellsBeyondHeight()
+        {
+            Field field = new Field(1, 1);
+            Assert.IsFalse(field.GetCell(0, 1));
+        }
+
+        [TestMethod]
+        public void FieldThrowsExceptionWhenSettingCellsBeyondWidth()
+        {
+            Field field = new Field(1, 1);
+            Assert.IsFalse(field.GetCell(1, 0));
+        }
+
+        [TestMethod]
+        public void FieldThrowsExceptionWhenSettingCellsBeyondHeight()
+        {
+            Field field = new Field(1, 1);
+            Assert.IsFalse(field.GetCell(0, 1));
+        }
+
+        [TestMethod]
+        public void FieldCanHaveCellSet()
+        {
+            Field field = new Field(1, 1);
+            field.SetCell(0, 0);
+            
+            Assert.IsTrue(field.GetCell(0, 0));
+        }
+
+        [TestMethod]
+        public void LonelyCellDies()
+        {
+            Field field = new Field(3, 3);
+            field.SetCell(1, 1);
+
+            field.Update();
+
+            Assert.IsFalse(field.GetCell(1, 1));
+        }
+
+        [TestMethod]
+        public void CellWithEnoughNeighboursLives()
+        {
+            Field field = new Field(3, 3);
+            field.SetCell(1, 1);    //Test cell
+            field.SetCell(0, 0);    //Neighbour
+            field.SetCell(1, 0);    //Neighbour
+
+            field.Update();
+
+            Assert.IsTrue(field.GetCell(1, 1));
+        }
+
+        [TestMethod]
+        public void OvercrowdedCellDies()
+        {
+            Field field = new Field(3, 3);
+            field.SetCell(1, 1);    //Test cell
+            field.SetCell(0, 0);    //Neighbour
+            field.SetCell(1, 0);    //Neighbour
+            field.SetCell(2, 0);    //Neighbour
+            field.SetCell(0, 1);    //Neighbour
+
+            field.Update();
+
+            Assert.IsFalse(field.GetCell(1, 1));
+        }
+
+        [TestMethod]
+        public void DeadCellWith3NeighboursLives()
+        {
+            Field field = new Field(3, 3);
+            field.SetCell(0, 0);    //Neighbour
+            field.SetCell(1, 0);    //Neighbour
+            field.SetCell(2, 0);    //Neighbour
+
+            field.Update();
+
+            Assert.IsTrue(field.GetCell(1, 1));
+        }
+    }
+}
